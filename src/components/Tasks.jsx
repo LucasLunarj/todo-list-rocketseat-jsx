be7cdf47230f5752446a45f2c287f27a68cc05c8
Luna <luna@pop-os.localdomain>
2023-08-25 23:27:12 -0300

    Remove and counter working

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -10,14 +10,14 @@ export function Tasks(props) {
 
 
   const tasks = props.listData.map((item, index) => {
-    return <TasksCreated key={item.id} taskData={item.task} receiveId={() => handleGetID(item.id, index)} />
+    return <TasksCreated key={item.id} taskData={item.task} receiveId={() => handleGetID(item.id)} />
   })
 
-  function handleGetID(id, index) {
-    props.idGenerator(id, index)
+  function handleGetID(id) {
+    props.idGenerator(id)
 
   }
-
+  console.log(props.tasksCreatedAmount)
   return (
     <div className={styles.container}>
       <div className={styles.content}>
@@ -36,7 +36,7 @@ export function Tasks(props) {
       <div>
 
       </div>
-      {props.taksCreatedAmount === 0 ? < EmptyTasks /> : tasks}
+      {props.tasksCreatedAmount === 0 ? < EmptyTasks /> : tasks}
 
     </div>
   );
